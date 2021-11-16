14f8772fdc9069f8e58da7d61889e8eba1bc0384
Fiorella Ruiz-Pace <fruizpace@gmail.com>
2021-11-16 23:19:44 +0100

    Boton delete y completed funcionan. Min 58:20 del video.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+// Importing Components
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [inputText, setInputText] = useState(""); // por aquí inyectamos los datos a la lista
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Fio's ToDo List</h1>
       </header>
+      <Form setInputText={setInputText} />
+      <TodoList />
     </div>
   );
 }
-
+/*
+1) pasamos con props la funcion setInputText al componente hijo Form.
+2) setInputText cambiará el estado de inputText de "" a "lo que sea que escriba en input"
+3) creamos un estado para la lista ToDo que será un array vacío []
+4)
+*/
 export default App;
